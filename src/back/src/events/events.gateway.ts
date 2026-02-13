5f412daede3fc31476dbd016fd28239fc291f7fc
a24amioulabi <a24amioulabi@inspedralbes.cat>
2026-02-13 12:22:45 +0100

    Afegir  WebSocket amb EventsGateway i actualitzar App module

---
+++ src/back/src/events/events.gateway.ts
@@ -0,0 +1,31 @@
+import { 
+  WebSocketGateway, 
+  WebSocketServer, 
+  OnGatewayInit, 
+  OnGatewayConnection, 
+  OnGatewayDisconnect 
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*', // IMPORTANTE: Permite que el Frontend se conecte desde otro puerto
+  },
+})
+export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  
+  @WebSocketServer()
+  server: Server;
+
+  afterInit(server: Server) {
+    console.log('✅ Socket Gateway inicializado');
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`🔌 Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Mq Cliente desconectado: ${client.id}`);
+  }
+}
