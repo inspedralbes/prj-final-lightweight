fbb65c8bb19f902f63a143312f5b8742629abe8e
David <davidquispe2109@gmail.com>
2026-02-19 10:16:42 +0100

    CRUD iniciat, sense exercicis posats. Falta la protecció del JWT i validar datos en DTO

--- src/back/src/app.module.ts
+++ src/back/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { EventsGateway } from './events/events.gateway'; // <--- 1. Importamos el archivo que acabas de crear
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { RoutineModule } from './routine/routine.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    RoutineModule,
   ],
   controllers: [AppController],
   providers: [
