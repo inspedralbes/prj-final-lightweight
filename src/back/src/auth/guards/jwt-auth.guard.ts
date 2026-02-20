aadd8cf4e0a252b216a853344dae59917b8ef306
a20valzavvas <92580502+a20valzavvas@users.noreply.github.com>
2026-02-20 22:51:03 +0100

    Revert "refactor: cambiar nombre módulo y servicio, actualizar modelo de datos y controlador para el manejo de nuevas sesiones (#12)" (#13)
    This reverts commit ab8711ad99faf016170acee881413c98f461abc1.

--- src/back/src/auth/guards/jwt-auth.guard.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-// Este guardse utiliza para proteger las rutas que requieren autenticación JWT.
-// Al extender AuthGuard con la estrategia 'jwt', se asegura de que solo los usuarios autenticados puedan acceder a las rutas protegidas.
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
